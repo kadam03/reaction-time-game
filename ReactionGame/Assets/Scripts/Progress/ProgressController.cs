bc40cff7eba39d9282376b7860133dc2324e6f45
kadam03 <62893104+kadam03@users.noreply.github.com>
2023-01-12 01:41:51 +0100

    Unity project update to 2022.2 and VS solution fix
    - There was an unnecessary using of the Unity Editor library which failed the build

--- ReactionGame/Assets/Scripts/Progress/ProgressController.cs
+++ ReactionGame/Assets/Scripts/Progress/ProgressController.cs
@@ -4,8 +4,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.Windows;
 
